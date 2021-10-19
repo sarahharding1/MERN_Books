0344bc5f48c33c3f95e609986d07d33e0f98acaf
sarahharding1 <G00374825@gmit.ie>
2021-10-19 10:50:44 +0100

    lab

--- src/Components/create.js
+++ src/Components/create.js
@@ -2,10 +2,99 @@ import React, { Component } from 'react';
 
 class Create extends Component{
 
+    constructor(){
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeMovieName = this.onChangeMovieName.bind(this);
+        this.onChangeYear = this.onChangeYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+    
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: ''
+            }
+    }
+        handleSubmit(event){
+            console.log(
+                "Name: "+this.state.Title
+                +"Year: "+this.state.Year
+                +"Poster: "+this.state.Poster);
+                event.preventDefault();
+                this.setState({
+                    Title: '',
+                    Year: '',
+                    Poster: ''
+                });
+
+
+        }
+        onChangeMovieName(event){
+            this.setState({
+                Title: event.target.value
+            })
+
+        }
+
+        onChangeYear(event){
+            this.setState({
+                Year: event.target.value
+            })
+
+        }
+
+        onChangePoster(event){
+            this.setState({
+                Poster: event.target.value
+            })
+
+        }
         render(){
             return(
                 <div>
-                    <h1>Create Component is in here</h1>
+                    
+                    <form onSubmit={this.handleSubmit} >
+                        {/* JSX code in here to field the input */}
+                        <div className="form-group">
+                            <label>Add Movie Name:  </label>
+                            <input type="text"
+                                className="form-control"
+                                value={this.state.Title}
+                                onChange={this.onChangeMovieName}
+                                
+                                /> 
+                       
+
+                        </div>
+                    
+                        <div className="form-group">
+                            <label>Add Year:  </label>
+                            <input type="text"
+                                className="form-control"
+                                value={this.state.Year}
+                                onChange={this.onChangeYear}
+                                
+                                /> 
+
+
+                        </div>
+
+                        <div className="form-group">
+                            <label>Add Poster:  </label>
+                            <input type="text"
+                                className="form-control"
+                                value={this.state.Poster}
+                                onChange={this.onChangePoster}
+                                
+                                /> 
+
+                        </div>
+                        <div>
+                            <input type="submit" value="Add "
+                                className="btn  btn-primary"></input>
+                        </div>
+                    </form>
+
                 </div>
             );
         }
--- src/Components/read.js
+++ src/Components/read.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
 import Movies from './movies';
+import axios from 'axios';
 
 class Read extends Component{
+
+        componentDidMount() {
+            // axios.get is called a promise
+            axios.get('https://jsonblob.com/api/jsonblob/894944504570986496')
+            .then((response)=>{ //arrow function
+                // allows us to set a state
+                this.setState({mymovies: response.data.movies})
+            })
+            .catch((error)=>{
+                console.log(error);
+            });
+;        }
     //stores JSON data
     state = { // 'state' used to represent data. here we have an object, inside an array, which is inside an object.
         mymovies: [
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg" },
+            // ** Hardcoded data. Replaced using API and JSON data
+            // {
+            // "Title": "Captain America: Civil War",
+            // "Year": "2016",
+            // "imdbID": "tt3498820",
+            // "Type": "movie",
+            // "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg" },
 
-            {
-            "Title": "Charlie Wilson's War", 
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg" },
+            // {
+            // "Title": "Charlie Wilson's War", 
+            // "Year": "2007",
+            // "imdbID": "tt0472062",
+            // "Type": "movie",
+            // "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg" },
 
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg" }
+            // {
+            // "Title": "Avengers: Infinity War",
+            // "Year": "2018",
+            // "imdbID": "tt4154756",
+            // "Type": "movie",
+            // "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg" }
         ]
     };
         // here we have the render method which is responsible for describing the view to be rendered to the browser window
