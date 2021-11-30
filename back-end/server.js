864b87314adfdb5a7bd938eb5f3d21537d08aef5
sarahharding1 <G00374825@gmit.ie>
2021-11-30 09:58:39 +0000

    lab10

--- back-end/server.js
+++ back-end/server.js
@@ -3,14 +3,21 @@ const app = express()
 const port = 4000
 const cors = require('cors');
 const bodyParser = require("body-parser");
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
 const mongoose = require('mongoose');
 
-app.use(cors());
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
-});
+
+// Not required anymore as we just have one domain
+// app.use(cors());
+// app.use(function(req, res, next) {
+// res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); res.header("Access-Control-Allow-Headers",
+// "Origin, X-Requested-With, Content-Type, Accept");
+// next();
+// });
 
 // Parse app
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -98,6 +105,10 @@ app.post('/api/movies', (req, res) =>{
     res.send('Item Added');
 
 })
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/../build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
