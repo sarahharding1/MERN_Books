832a28d1d1a1e44f605a1e846e790e8d343f68dc
sarahharding1 <G00374825@gmit.ie>
2021-12-17 11:15:45 +0000

    updated cors functionality

--- back-end/server.js
+++ back-end/server.js
@@ -10,6 +10,14 @@ app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
 const mongoose = require('mongoose');
 
+// Not required anymore as we just have one domain
+app.use(cors());
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); res.header("Access-Control-Allow-Headers",
+"Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
+
 
 // Parse app
 app.use(bodyParser.urlencoded({ extended: false }))
